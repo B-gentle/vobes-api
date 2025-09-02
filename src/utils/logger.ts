59f84938f6add2fe93a1428b27a14c540991762b
b-gentle <brighteyo16@gmail.com>
2025-09-02 09:57:57 +0100

    changed logger to console only

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,40 +1,12 @@
-import { createLogger, transports, format } from 'winston';
-import { env } from '../commons/env.js';
-const { combine, timestamp, prettyPrint, errors } = format;
+import winston from "winston";
 
-const defaultLogLevel = env.LOG_LEVEL;
-const errorLogLevel = 'warn';
-
-const devTransports = new transports.Console({
-  level: defaultLogLevel,
-});
-
-const prodTransports = [
-  new transports.Console({
-    level: defaultLogLevel,
-  }),
-
-  new transports.File({
-    filename: 'logs/combined.log',
-    level: defaultLogLevel,
-  }),
-
-  new transports.File({
-    filename: 'logs/errors.log',
-    level: errorLogLevel,
-  }),
-];
-
-const logTransports =
-  env.NODE_ENV === 'test'
-    ? []
-    : env.NODE_ENV === 'production'
-      ? prodTransports
-      : devTransports;
-
-export const logger = createLogger({
-  transports: logTransports,
-  format: combine(errors({ stack: true }), timestamp(), prettyPrint()),
-  exceptionHandlers: [new transports.File({ filename: 'logs/exceptions.log' })],
-  rejectionHandlers: [new transports.File({ filename: 'logs/rejections.log' })],
+export const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console(),
+  ],
 });
